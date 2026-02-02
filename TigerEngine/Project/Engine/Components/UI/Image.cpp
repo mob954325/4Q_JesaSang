e21ccacd9b3057017169e2a7fb54a506bb62e6d3
mob954325 <lsh954325@gmail.com>
2026-02-02 15:21:48 +0900

    Feat | Image 컴포넌트 추가
    - 마우스 이벤트는 아직 없음

--- TigerEngine/Project/Engine/Components/UI/Image.cpp
+++ TigerEngine/Project/Engine/Components/UI/Image.cpp
@@ -1,10 +1,41 @@
 #include "Image.h"
+#include "../../Manager/TextureResourceManager.h"
+#include "../../Object/GameObject.h"
 
 void Image::GetTextureByPath(std::string path)
 {
-
+    resource = TextureResourceMaager::Instance().LoadTextureResourceByPath(path);
 }
 
 void Image::OnRender(RenderQueue& queue)
 {
+    if (!resource) return;
+
+    // ItemData 채우기
+    UIRenderItem data;
+    data.color = color;
+
+    Vector2 rectSize = rect->GetSize();
+    data.imageSize = Vector4(rectSize.x, rectSize.y, resource->texSizePx.x, resource->texSizePx.y);
+
+    data.params = Vector4((float)type, fillAmount, 0.0f, 0.0f);
+
+    data.uvRect = sliceBorderPx;
+
+    queue.AddUIRenderQueue(data); // NOTE : 이러면 sort를 renderpass에서 처리하면되니까 canvas는 왜 필요한거지
+}
+
+void Image::SetMouseCheck(bool value)
+{
+    isMouseCheck = value;
+}
+
+bool Image::GetMouseCheck() const
+{
+    return isMouseCheck;
+}
+
+void Image::Init()
+{
+    rect = GetOwner()->AddComponent<RectTransform>();
 }
--- TigerEngine/Project/Engine/Components/UI/Image.h
+++ TigerEngine/Project/Engine/Components/UI/Image.h
@@ -1,11 +1,49 @@
 #pragma once
 #include "../RenderComponent.h"
+#include "../../Manager/UIData/TextureResource.h"
+#include "../../Components/RectTransform.h"
 
+enum class ImageType
+{
+    Simple,		// 기본 이미지 출력
+    Sliced,		// 이미지 중간 부분만 늘려지는거 ( 9-Sliced는 아님 )
+    Fill		// 이미지 채우기
+};
+
+/// <summary>
+/// UI 이미지 컴포넌트 
+/// GameObject에 RectTransform을 추가한다. -> Transform, RectTransform 둘 다 가지고 있음.
+/// </summary>
+/// <remarks>
+/// Transform과 RectTransform 둘 다 가지고 있는데 대게 스크린 기준 좌표계는 rect의 pos를 사용한다.
+/// </remarks>
 class Image : public RenderComponent
 {
     RTTR_ENABLE(RenderComponent)
 public:
-    void GetTextureByPath(std::string path);
+    Image() { Init(); }
+    ~Image() = default;
 
+    void GetTextureByPath(std::string path);
     void OnRender(RenderQueue& queue) override;
+
+    void SetMouseCheck(bool value);
+    bool GetMouseCheck() const;
+
+private:
+    void Init();
+
+    std::shared_ptr<TextureResource> resource{};
+    RectTransform* rect;
+
+    ImageType type = ImageType::Simple;
+    Color color{};
+
+    // fillMode에서 사용할 값
+    float fillAmount = 1.0f;
+
+    // 9-slice 보더 px (L, R, T, B)
+    Vector4 sliceBorderPx{ 0,0,0,0 }; 
+
+    bool isMouseCheck = false;
 };
