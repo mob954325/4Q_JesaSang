083572c910a750075a64c5fb64b888a9e685d017
mob954325 <lsh954325@gmail.com>
2026-02-02 15:22:26 +0900

    Feat | UIRenderQueue Getter / Setter / Clear 추가

--- TigerEngine/Project/Engine/RenderQueue/RenderQueue.h
+++ TigerEngine/Project/Engine/RenderQueue/RenderQueue.h
@@ -49,7 +49,7 @@ private:
     // Render Queue
     std::vector<RenderItem> opaqueQueue;        // 불투명 오브젝트 -> Deffered Rendering
     std::vector<RenderItem> transparentQueue;   // 투명 오브젝트   -> Forward Rendering
-    std::vector<UIRenderItem> UIRenderQueue;    // UI 오브젝트 -> ui 렌더링에 필요한 내용 따로 queue 추가함 - 26.02.02 이성호
+    std::vector<UIRenderItem> uiRenderQueue;    // UI 오브젝트 -> ui 렌더링에 필요한 내용 따로 queue 추가함 - 26.02.02 이성호
 
 public:
     void AddOpaqueQueue(const RenderItem& item)
@@ -62,13 +62,20 @@ public:
         transparentQueue.push_back(item);
     }
 
+    void AddUIRenderQueue(const UIRenderItem& item)
+    {
+        uiRenderQueue.push_back(item);
+    }
+
     const auto& GetOpaqueQueue()  const { return opaqueQueue; }
     const auto& GetTransparentQueue()  const { return transparentQueue; }
+    const auto& GetUIRenderQueue() const { return uiRenderQueue; }
 
     void Clear()
     {
         opaqueQueue.clear();
         transparentQueue.clear();
+        uiRenderQueue.clear();
     };
 };
 
