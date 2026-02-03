e278e2b5f730708c07701853e01cf09ed8ae437c
mob954325 <lsh954325@gmail.com>
2026-02-03 09:40:07 +0900

    Add | Enable 주석 내용 추가

--- TigerEngine/Project/Engine/Object/Enableable.h
+++ TigerEngine/Project/Engine/Object/Enableable.h
@@ -5,6 +5,11 @@
 /// <summary>
 /// 활성화 비활성화 여부를 가지고 있는 클래스
 /// </summary>
+/// <remarks>
+/// Unity에서 GameObject는 SetActive, Component는 isEnable로 체크하지만 
+/// 현재 프로젝트에서 나눌 이유는 없을거 같아서 해당 클래스를 상속받도록함. [02.03]
+/// 용어가 Enable, Active가 혼용되어 있지만, 객체의 활성화 비활성화 여부만 확인한다.
+/// </remarks>
 class Enableable : public Object
 {
     RTTR_ENABLE()
