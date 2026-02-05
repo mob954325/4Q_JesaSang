88de44ad91ab3086238b0442d5a3fcc1d5cdfdb9
Lee SeongHo <lsh954325@gmail.com>
2026-02-06 00:57:49 +0900

    Feat | 인코딩 컨버터 헬퍼 추가

---
+++ TigerEngine/Project/Engine/Util/EncodeConvertHelper.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <windows.h>
+
+static std::string WStringToUtf8(const std::wstring& w)
+{
+    if (w.empty()) return {};
+    int size = WideCharToMultiByte(CP_UTF8, 0, w.c_str(), (int)w.size(), nullptr, 0, nullptr, nullptr);
+    std::string out(size, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, w.c_str(), (int)w.size(), out.data(), size, nullptr, nullptr);
+    return out;
+}
+
+// UTF-8 string -> wstring(UTF-16)
+static std::wstring Utf8ToWString(const std::string& s)
+{
+    if (s.empty()) return {};
+    int size = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), (int)s.size(), nullptr, 0);
+    std::wstring out(size, L'\0');
+    MultiByteToWideChar(CP_UTF8, 0, s.c_str(), (int)s.size(), out.data(), size);
+    return out;
+}
