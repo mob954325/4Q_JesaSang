0778e8eebeed478e2e4400904c0d6cbc43213a25
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2026-02-06 20:29:37 +0900

    Feat | update

--- TigerEngine/Project/WorkSpace/02_Script/Woo/Player/PlayerController.cpp
+++ TigerEngine/Project/WorkSpace/02_Script/Woo/Player/PlayerController.cpp
@@ -189,30 +189,34 @@ void PlayerController::LoadAnimation()
 {
     // 애니메이션 파일 로드
     FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_idle_character.glb", "Idle");
-    FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_walk_character.glb", "Walk");
-    FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_run_character.glb", "Run");
-    FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_sit_character.glb", "Sit");
-    FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_cry_character.glb", "Hit");
+    //FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_walk_character.glb", "Walk");
+    //FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_run_character.glb", "Run");
+    //FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_sit_character.glb", "Sit");
+    //FBXResourceManager::Instance().LoadAnimationByPath(fbxData->GetFBXInfo(), "..\\Assets\\Resource\\FBX\\00_character\\Player\\ani_cry_character.glb", "Hit");
 
     // 클립 생성
     auto idleClip = animController->FindClip("Idle");
-    auto walkClip = animController->FindClip("Walk");
-    auto runClip = animController->FindClip("Run");
-    auto sitClip = animController->FindClip("Sit");
-    auto hitClip = animController->FindClip("Hit");
+    //auto walkClip = animController->FindClip("Walk");
+    //auto runClip = animController->FindClip("Run");
+    //auto sitClip = animController->FindClip("Sit");
+    //auto hitClip = animController->FindClip("Hit");
 
-    if (!idleClip || !walkClip || !runClip || !sitClip || !hitClip)
+    if (!idleClip/* || !walkClip || !runClip || !sitClip || !hitClip*/)
     {
         cout << "[Player Animation] Clip not found!\n" << endl;
         return;
     }
+    else
+    {
+        cout << "[Player] Animation Load Success" << endl;
+    }
 
     // 상태 등록
     animController->AddState(std::make_unique<AnimationState>("Idle", idleClip, animController));
-    animController->AddState(std::make_unique<AnimationState>("Walk", walkClip, animController));
-    animController->AddState(std::make_unique<AnimationState>("Run", runClip, animController));
-    animController->AddState(std::make_unique<AnimationState>("Sit", sitClip, animController));
-    animController->AddState(std::make_unique<AnimationState>("Hit", hitClip, animController));
+    //animController->AddState(std::make_unique<AnimationState>("Walk", walkClip, animController));
+    //animController->AddState(std::make_unique<AnimationState>("Run", runClip, animController));
+    //animController->AddState(std::make_unique<AnimationState>("Sit", sitClip, animController));
+    //animController->AddState(std::make_unique<AnimationState>("Hit", hitClip, animController));
 }
 
 /*-------[ Init ]-------------------------------------*/
